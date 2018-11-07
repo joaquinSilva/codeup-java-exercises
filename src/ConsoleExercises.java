41656814ed71767ba000b8919b17f0792ca8d659
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-07 16:42:41 -0600

    created and completed several exercise files for into to java.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        double pi = 3.14159;
+        System.out.println(pi);
+        System.out.format("The value of pi is approximately %.2f%n", pi);
+
+        Scanner sc = new Scanner(System.in);
+
+//        System.out.println("Enter a number!");
+//        int userNum = sc.nextInt();
+//        System.out.println("You entered " + userNum + "!");
+
+
+//        System.out.println("Enter 3 random words!");
+//        String firstWord = sc.next();
+//        String secondWord = sc.next();
+//        String thirdWord = sc.next();
+//
+//        System.out.println(firstWord);
+//        System.out.println(secondWord);
+//        System.out.println(thirdWord);
+////
+//        System.out.printf("%s%n%s%n%s%n", firstWord, secondWord, thirdWord);
+//        sc.nextLine();
+//        System.out.println("Enter a complete sentence!");
+//        String userSentence = sc.nextLine();
+//        System.out.println(userSentence);
+//        sc.nextLine();
+
+//        System.out.println("Enter the length and width of a Codeup Classroom!");
+////        String length = sc.nextLine();
+////        String width = sc.nextLine();
+//        int length = Integer.parseInt(sc.nextLine());
+//        int width = Integer.parseInt(sc.nextLine());
+//        int area = length * width;
+//        int perimeter = length*2 + width*2;
+//        System.out.println("The area of this Codeup Classroom is " + area + "!");
+//        System.out.println("The perimeter of this Codeup Classroom is " + perimeter + "!");
+//        sc.nextLine();
+//        System.out.format("The area is %d and the perimeter is %d", area, perimeter);
+
+
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,46 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, ");
+        System.out.println("world!");
+        int myFavoriteNumber = 3;
+        System.out.println(myFavoriteNumber);
+        String myString = "hello superstar!";
+        System.out.println(myString);
+        float myNumber = (float) 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        System.out.println(x);
+        y -= x;
+        System.out.println(y);
+
+        byte age = 127;
+        age++;
+        System.out.println(age);
+
+    }
+}
