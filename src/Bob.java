2915e10daa3a9fb92a2deca6d7d3c2a2c9e07592
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-08 15:14:24 -0600

    completed the string exercise and created the Bob string exercise file.

---
+++ src/Bob.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        
+
+
+
+    }
+}
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -6,10 +6,23 @@ public class StringExercise {
 
         Scanner sc = new Scanner(System.in);
 
+//        String message = "We don't need no education\nWe don't need no thought control";
+//        System.out.println(message);
 
+//        String message = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(message);
 
+//        String message = "In windows, the main drive is actually C:\\";
+//        System.out.println(message);
 
+//        String message = "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+//        System.out.println(message);
 
+        String message = "\nWe don't need no education\nWe don't need no thought control\n\n";
+        message += "Check \"this\" out!, \"s inside of \"s!\n\n";
+        message += "In windows, the main drive is actually C:\n\n";
+        message += "I can do backslashes \\, double backslashes \\\\,\nand the amazing triple backslash \\\\\\!";
+        System.out.println(message);
 
     }
 }
