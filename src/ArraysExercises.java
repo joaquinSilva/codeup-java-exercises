b218e61b08e83d2662ce675a4411e6b350f7ff03
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-14 16:31:26 -0600

    created and completed server name generator exercise and completed arrays exercise.

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -2,6 +2,16 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
+//    Create a static method named addPerson. It should accept an array of Person objects, as well as a single person object to add to the passed array. It should return an array whose length is 1 greater than the passed array, with the passed person object at the end of the array.
+
+
+    public static Person[] addPerson(Person[] arrayofPeople, Person aPerson) {
+        Person[] newArray = Arrays.copyOf(arrayofPeople, arrayofPeople.length + 1);
+        newArray[newArray.length - 1] = aPerson;
+        return newArray;
+
+    }
+
     public static void main(String[] args) {
 
 //        int[] numbers = {1, 2, 3, 4, 5};
@@ -13,20 +23,30 @@ public class ArraysExercises {
             people[i] = new Person("defaultName");
         }
 
-        System.out.println(people[0]);
-
-
-        System.out.println(Arrays.toString(people));
+//        System.out.println(people[0]);
+//
+//
+//        System.out.println(Arrays.toString(people));
 
         people[0].setName("Luke Skywalker");
         people[1].setName("Han Solo");
         people[2].setName("Jabba the Hutt");
 
-        System.out.println(people[0].getName());
+//        System.out.println(people[0].getName());
 
-        for (Person person : people ) {
+//        for (Person person : people ) {
+//            System.out.println(person.getName());
+//        }
+
+        Person c3po = new Person("C3P0");
+
+//        System.out.println(Arrays.toString(addPerson(people, c3po)));
+
+        Person[] newArray = (addPerson(people, c3po));
+        for (Person person : newArray) {
             System.out.println(person.getName());
         }
+
 
     }
 
