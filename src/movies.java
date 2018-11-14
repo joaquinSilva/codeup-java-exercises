f5753a7dab67b5dc4b927b10b141f1fe3e0a0dfc
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-14 16:43:02 -0600

    created movies class

---
+++ src/movies.java
@@ -0,0 +1,28 @@
+public class movies {
+
+    public static void main(String[] args) {
+
+
+    }
+
+//    Movie List
+//
+//    We are going to build an application that keeps track of movies and displays them by category.
+//
+//    Inside of src, create a directory named movies. Create all of the classes described below in this directory.
+//    Create a class named Movie. It should have private fields for name and category, and a constructor that sets both of these. Create methods to access these properties and change them (getters and setters).
+//    Download the MoviesArray file and save it as MoviesArray.java inside of movies. This class has a static method named findAll that will return an array of Movie objects.
+//    Create a class named MoviesApplication that has a main method.
+//    Give the user a list of options for viewing all the movies or viewing movies by category.
+//    Use your Input class to get input from the user, and display information based on their choice. (Remember to import your Input class)
+//    If a category is selected, only movies from that category should be displayed.
+//    Your application should continue to run until the user chooses to exit.
+
+
+    public static void movie {
+        
+    }
+
+
+
+}
