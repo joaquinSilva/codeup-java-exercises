2e94485836d35f058fc0ae18f6b2f68f8cc28d01
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-15 11:43:20 -0600

    completed base exerises for movie application.

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -2,46 +2,21 @@ package movies;
 
 import util.Input;
 
+import java.util.Scanner;
+
 public class MoviesApplication {
 
+    public static Movie[] allMoviesArray = MoviesArray.findAll();
+
     public static void main(String[] args) {
-
         init();
-
-
     }
 
     private static void init() {
 //        add conditional if else statements here to give prompts and run methods in response to user inputs
 
-        System.out.format("%nHello! Welcome to our movie database. What would you like to do?%n%n" +
-                "      0 - exit\n" +
-                "      1 - view all movies\n" +
-                "      2 - view movies in the animated category\n" +
-                "      3 - view movies in the drama category\n" +
-                "      4 - view movies in the horror category\n" +
-                "      5 - view movies in the scifi category\n" +
-                "      6 - add movie");
+        useMenu();
 
-
-
-
-//        Input.getInt();
-
-
-
-
-//        Movie[] allMoviesArray = MoviesArray.findAll();
-//
-//        showAllMovies(allMoviesArray);
-//
-//        showAnimatedCategory(allMoviesArray);
-//
-//        showDramaCategory(allMoviesArray);
-//
-//        showHorrorCategory(allMoviesArray);
-//
-//        showScifiCategory(allMoviesArray);
     }
 
 
@@ -73,55 +48,73 @@ public class MoviesApplication {
 //    Add functionality to allow a user to add a new movie to the list.
 //...
 
-    public static void showAllMovies(Movie[] movieArray) {
+//This overloaded method shows all movies if only a movie array is passed as an argument.
+    public static void showMovies(Movie[] movieArray) {
         for (Movie movie : movieArray) {
             System.out.format("%n%s -- %s", movie.getCategory(), movie.getName());
         }
     }
 
 
-    public static void showAnimatedCategory(Movie[] movieArray) {
-        int animated = 0;
+//This overloaded method shows all the movies from a specific category if a category is passed in as a 2nd argument.
+    public static void showMovies(Movie[] movieArray, String category) {
         for (Movie movie : movieArray) {
-            if (movie.getCategory().equals("animated")) {
+            if (movie.getCategory().equals(category)) {
                 System.out.format("%n%s -- %s", movie.getCategory(), movie.getName());
             }
         }
     }
 
+    public static String showMenu() {
 
-    public static void showDramaCategory(Movie[] movieArray) {
-        int animated = 0;
-        for (Movie movie : movieArray) {
-            if (movie.getCategory().equals("drama")) {
-                System.out.format("%n%s -- %s", movie.getCategory(), movie.getName());
-            }
-        }
+        return ("%n%nHello! What would you like to do with our movie database?%n%n" +
+                "      0 - exit\n" +
+                "      1 - view all movies\n" +
+                "      2 - view movies in the animated category\n" +
+                "      3 - view movies in the drama category\n" +
+                "      4 - view movies in the horror category\n" +
+                "      5 - view movies in the scifi category\n" +
+                "      6 - view movies in the musical category\n" +
+                "      7 - add movie%n");
     }
 
-    public static void showHorrorCategory(Movie[] movieArray) {
-        int animated = 0;
-        for (Movie movie : movieArray) {
-            if (movie.getCategory().equals("horror")) {
-                System.out.format("%n%s -- %s", movie.getCategory(), movie.getName());
-            }
+    public static void useMenu() {
+        System.out.format(showMenu());
+        Scanner sc = new Scanner(System.in);
+        Input input = new Input(sc);
+        int userInput = input.getInt();
+
+
+        if (userInput == 0) {
+            System.out.println("Goodbye");
+        } else if (userInput == 1) {
+            showMovies(allMoviesArray);
+            useMenu();
+        } else if (userInput == 2) {
+            showMovies(allMoviesArray, "animated");
+            useMenu();
+        } else if (userInput == 3) {
+            showMovies(allMoviesArray, "drama");
+            useMenu();
+        } else if (userInput == 4) {
+            showMovies(allMoviesArray, "horror");
+            useMenu();
+        } else if (userInput == 5) {
+            showMovies(allMoviesArray, "scifi");
+            useMenu();
+        } else if (userInput == 6) {
+            showMovies(allMoviesArray, "musical");
+            useMenu();
+        } else if (userInput == 7) {
+//            addNewMovie();
+            useMenu();
         }
+
     }
 
-    public static void showScifiCategory(Movie[] movieArray) {
-        int animated = 0;
-        for (Movie movie : movieArray) {
-            if (movie.getCategory().equals("scifi")) {
-                System.out.format("%n%s -- %s", movie.getCategory(), movie.getName());
-            }
-        }
-    }
-
-
-//
-//    public static Movie[] addNewMovie(Person[] arrayofPeople, Person aPerson) {
-//        Person[] newArray = Arrays.copyOf(arrayofPeople, arrayofPeople.length + 1);
-//        newArray[newArray.length - 1] = aPerson;
+//    public static Movie[] addNewMovie(Movie[] arrayofMovies, Movie aNewMovie) {
+//        allMoviesArray = Arrays.copyOf(arrayofMovies, arrayofMovies.length + 1);
+//        newArray[newArray.length - 1] = aNewMovie;
 //        return newArray;
 //
 //    }
