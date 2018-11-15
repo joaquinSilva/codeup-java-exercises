75e7bd1685483b1817219b824934564111d2c30a
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2018-11-15 15:52:36 -0600

    worked on bonus for movie application class.

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -9,44 +9,8 @@ public class MoviesApplication {
     public static Movie[] allMoviesArray = MoviesArray.findAll();
 
     public static void main(String[] args) {
-        init();
-    }
-
-    private static void init() {
-//        add conditional if else statements here to give prompts and run methods in response to user inputs
-
         useMenu();
-
     }
-
-
-//    Use your Input class to get input from the user, and display information based on their choice.
-//    (Remember to import your Input class)
-//    If a category is selected, only movies from that category should be displayed.
-//    Your application should continue to run until the user chooses to exit.
-//
-
-//
-//    What would you like to do?
-//
-//      0 - exit
-//      1 - view all movies
-//      2 - view movies in the animated category
-//      3 - view movies in the drama category
-//      4 - view movies in the horror category
-//      5 - view movies in the scifi category
-//      6 - add movie
-//
-//    Enter your choice: 1
-//
-//    Frankenstein -- horror
-//    Goodfellas -- drama
-//    Pulp Fiction -- drama
-
-    //    Bonus
-//
-//    Add functionality to allow a user to add a new movie to the list.
-//...
 
 //This overloaded method shows all movies if only a movie array is passed as an argument.
     public static void showMovies(Movie[] movieArray) {
@@ -119,7 +83,33 @@ public class MoviesApplication {
 //
 //    }
 
+//    Use your Input class to get input from the user, and display information based on their choice.
+//    (Remember to import your Input class)
+//    If a category is selected, only movies from that category should be displayed.
+//    Your application should continue to run until the user chooses to exit.
+//
 
+//
+//    What would you like to do?
+//
+//      0 - exit
+//      1 - view all movies
+//      2 - view movies in the animated category
+//      3 - view movies in the drama category
+//      4 - view movies in the horror category
+//      5 - view movies in the scifi category
+//      6 - add movie
+//
+//    Enter your choice: 1
+//
+//    Frankenstein -- horror
+//    Goodfellas -- drama
+//    Pulp Fiction -- drama
+
+    //    Bonus
+//
+//    Add functionality to allow a user to add a new movie to the list.
+//...
 
 
 
